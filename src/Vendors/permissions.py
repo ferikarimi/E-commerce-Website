803f8aecc90face1c679e197477a452d05a7b7e7
ferikarimi <farzad3467@mail.com>
2025-04-18 05:58:40 +0330

    permmisions.py added to project

---
+++ src/Vendors/permissions.py
@@ -0,0 +1,32 @@
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+from .models import Vendors
+
+
+class IsVendorOwner (permissions.BasePermission):
+    def has_permission(self, request, view):
+        try :
+            return request.user.is_authenticated and Vendors.objects.get(user=request.user).role == 'owner'
+        except Vendors.DoesNotExist :
+            return False
+        
+
+class IsVendorManager (permissions.BasePermission):
+    def has_permission(self, request, view):
+        try :
+            return request.user.is_authenticated and Vendors.objects.get(user=request.user).role == 'manager'
+        except Vendors.DoesNotExist :
+            return False
+        
+
+class IsVendorOperator (permissions.BasePermission):
+    def has_permission(self, request, view):
+        try :
+            return request.user.is_authenticated and Vendors.objects.get(user=request.user).role == 'operator'
+        except Vendors.DoesNotExist :
+            return False
+        
+
+class IsAuthenticatedVendor (BasePermission) :
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_vendor   
