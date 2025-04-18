3cb614279fc3efdf98a0322854c000e1d50bed34
ferikarimi <farzad3467@mail.com>
2025-04-18 05:38:37 +0330

    account and customer models.py updated .

--- src/Accounts/models.py
+++ src/Accounts/models.py
@@ -3,17 +3,16 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class Customers (AbstractUser):
-    email = models.CharField(unique=True)
+class User (AbstractUser):
+
     phone_number = PhoneNumberField(null=True , blank=True) # بعدا اجباری میشه تو فاز های بعدی
     birth_date = models.DateField(null=True , blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True , blank=True)
     is_vendor = models.BooleanField(default=False)
     is_customer = models.BooleanField(default=True)
 
     class Meta:
-        db_table='customers'
+        db_table='User'
     
     def __str__(self):
         return f"{self.username}"
--- src/Customers/models.py
+++ src/Customers/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from Accounts.models import Customers
+from Accounts.models import User
 
 # Create your models here.
 # class Customers (User):
@@ -14,12 +14,11 @@ from Accounts.models import Customers
     
 
 class Addresses (models.Model):
-    customer = models.ForeignKey(Customers , on_delete=models.CASCADE, related_name='addresses')
+    user = models.ForeignKey(User , on_delete=models.CASCADE, related_name='addresses')
     city = models.CharField(max_length=100)
     address = models.TextField()
 
     def __str__(self):
         return f"address : {self.city} : {self.address}"
     
-
 
