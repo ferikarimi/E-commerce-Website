6f21575404ff6def3a25f8f2ad8d2d9812936aa9
ferikarimi <farzad3467@mail.com>
2025-05-01 02:32:05 +0330

    app 'Website' updated successfully

--- src/Website/urls.py
+++ src/Website/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import AllShopView , SingleShopView , GetShopProductView
+
 
 urlpatterns=[
-    path('all_shop/' , AllShopView.as_view() , name='all_shop'),
-    path('single_shop/<int:id>/' , SingleShopView.as_view() , name='single_shop'),
-    path('get_shop_product/<int:id>/' ,GetShopProductView.as_view() , name='get_shop_product'),
+
 ]
+
--- src/Website/views.py
+++ src/Website/views.py
@@ -1,68 +1,9 @@
-from django.shortcuts import render , redirect
-from Products.models import Shop ,Product
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination
-from .serializers import AllShopSerializer , SingleShopSerializer , ShowOneShopProductsSerializer
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated , IsAdminUser , AllowAny
-from django.shortcuts import get_object_or_404
-from rest_framework.pagination import PageNumberPagination
-from rest_framework import generics
-
-
-class ShopPagination (PageNumberPagination):
-    page_size = 6
-    page_size_query_param = 'page_size'
-    max_page_size = 12
-    
-
-class AllShopView(generics.ListAPIView):
-    permission_classes = [AllowAny]
-    shops = Shop.objects.all()
-    serializer_class = AllShopSerializer
-    pagination_class = ShopPagination
-
-    def get_queryset(self):
-        shops = Shop.objects.all()
-        sort_by = self.request.query_params.get('sort')
-
-        if sort_by == 'badseller':
-            shops = shops.order_by('product_sold_count')
-
-        elif sort_by == 'bestseller':
-            shops = shops.order_by('-product_sold_count')
-
-        elif sort_by == 'oldest':
-            shops = shops.order_by('created_at')
-        
-        else :
-            shops = shops.order_by('-created_at')
-        
-        return shops
-
-
-class SingleShopView(APIView):
-    permission_classes = [AllowAny]
-
-    def get (self , request , id):
-        shop = get_object_or_404(Shop , id=id)
-        serializer = SingleShopSerializer (shop)
-        return Response (serializer.data , status=200)
-
-class GetShopProductView (APIView):
-    permission_classes = [AllowAny]
-
-    def get (self , request , id):
-        shop = get_object_or_404(Shop ,id=id)
-        products = Product.objects.filter(store_name=shop)
-        serializer = ShowOneShopProductsSerializer (products , many=True)
-        return Response (serializer.data , status=200)
+from django.shortcuts import render 
 
 
 
 
-
+#_____________________________________ OTP __________________________________
 
 # from django.http import HttpResponseBadRequest
 # from .urls import create_token_view
