784524a00f46970ffbe01b780aede23c701c1cb8
ferikarimi <farzad3467@mail.com>
2025-05-08 20:33:11 +0330

    [200~Two extra apps were removed from the project.

--- src/Website/redis_setup.py
+++
@@ -1,2 +0,0 @@
-import redis
-client = redis.Redis(host="locaalhost" , port=6379 , db=0)
--- src/Website/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_tasks
-import requests
-from celery.backends.database import retry
-
-
-@shared_tasks(retry=10)
-def send_otp_code(otp_code):
-    requests.get(f"http://127.0.0.1:8001/show_code/{otp_code}")
